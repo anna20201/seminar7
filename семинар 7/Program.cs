59ee48032826bc8d1da534f796cf44057880915e
Анна <87242449+anna20201@users.noreply.github.com>
2024-05-03 16:59:28 +0300

    задача 1
    п

---
+++ семинар 7/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main ()
+    {
+        Console.WriteLine("введите значение M: ");
+        int M = int.Parse(Console.ReadLine());
+        
+
+        Console.WriteLine("введите значение N: ");
+        int N = int.Parse(Console.ReadLine());
+    
+     PrintNaturalNumbers (M,N);
+    }
+    static void PrintNaturalNumbers (int current, int N)
+{
+    if(current <=N)
+    {
+        Console.Write(current + " ");
+        PrintNaturalNumbers (current +1, N);
+    }
+}
+}
